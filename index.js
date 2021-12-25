09b4ee046a98d4d1b9fd572039533679e20ac0b3
Chelsea Li <chelseali2001@gmail.com>
2021-12-24 18:08:28 -0800

    Finished product

--- index.js
+++ index.js
@@ -11,40 +11,52 @@ let recipes = [
   },
   {
     name: "Omelet",
-    ingredients: [["eggs", "2", ""]],
-    instructions: ["1. Cook the eggs"]
+    ingredients: [["eggs", "2", ""], ["bell pepper", "1", ""]],
+    instructions: ["1. Cook the eggs", "2. Add in the bell peppers", "3. Enjoy"]
   }
 ];
 
 let ingredientsList = {};
+let recipesList = {};
 var selectedRow = 0;
 
 function getAllInfo(data) {
-  var existing = [];
+  var existingIngredients = [];
+  var existingRecipes = []
 
   for (let element of data) {
-    let list = Object.values(element["ingredients"]);
+    var list = Object.values(element["ingredients"]);
+    var list0 = Object.values(element["name"]);
 
     for (let x of list) {
       var exists = false;
 
-      for (let i in existing) {
-        if (x[0] == existing[i]) {
+      for (let i in existingIngredients) {
+        if (x[0] == existingIngredients[i]) {
           exists = true;
           break;
         }
       }
 
       if (!exists) {
-        existing.push(x[0])
+        existingIngredients.push(x[0]);
       }
     }
-  }
-  
-  existing.sort();
 
-  for (let element of existing) {
+    for (let x of list0) {
+      existingRecipes.push(x);
+    }
+  }
+
+  existingIngredients.sort();
+  existingRecipes.sort()
+
+  for (let element of existingIngredients) {
     ingredientsList[element] = true;
+  }
+
+  for (let element of existingRecipes) {
+    recipesList[element] = true;
   }
 }
 
@@ -66,7 +78,7 @@ function generateTable(data) {
   }
 
   for (let element in data) {
-    let row = "";
+    var row = "";
 
     if (data[element] == true) {
       row = ingredientsParent.insertRow();
@@ -134,18 +146,18 @@ function generateTable(data) {
   }
 }
 
-function search_ingredients(data) {
-  var input = document.getElementById("ingredient_search");
+function search(searchVal, data) {
+  var input = document.getElementById(searchVal);
   var filter = input.value.toLowerCase();
   var table = document.getElementById(data);
   var tr = table.getElementsByTagName("tr");
-
+  console.log(filter);
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     var td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       var txtValue = td.textContent || td.innerText;
-      if (txtValue.indexOf(filter) > -1) {
+      if (txtValue.toLowerCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
@@ -172,8 +184,98 @@ document.getElementById("delete-button").onclick = function() {
   generateTable(ingredientsList);
 }
 
-document.getElementById("find-recipes").onclick ="window.location.href='https://chelseali2001.github.io/RecipeFinder/recipes/possible_recipes';";
+document.getElementById("find-recipes-button").onclick = function() {
+  var possibleRecipesParent = document.getElementById("possible-recipes");
+
+  while (possibleRecipesParent.rows.length > 1) {
+    possibleRecipesParent.deleteRow(1);
+  }
+
+  var cells = possibleRecipesParent.getElementsByTagName('td');
+
+  for (let element of recipes) {
+    var list = Object.values(element["ingredients"]);
+    var add = true;
+
+    for (let x of list) {
+      if (ingredientsList[x[0]] == true) {
+        add = false;
+        break;
+      }
+    }
+
+    if (add == true) {
+      let row = possibleRecipesParent.insertRow();
+      let col = row.insertCell();
+      col.appendChild(document.createTextNode(element["name"]));
+    }
+  }
+
+  if (possibleRecipesParent.rows.length == 1) {
+    alert("There aren't any recipes you can make with these ingredients.");
+  } else {
+    document.getElementById("first-page").style.display = "none";
+    document.getElementById("second-page").style.display = "block";
+
+    for (var i = 0; i < cells.length; i++) {
+      // Take each cell
+      var cell = cells[i];
+      // do something on onclick event for cell
+      cell.onclick = function () {
+        // Get the row id where the cell exists
+        var rowId = this.parentNode.rowIndex;
+        var rowSelected = possibleRecipesParent.getElementsByTagName('tr')[rowId];
+        var selectedRecipe = rowSelected.innerHTML.split("<td>");
+        selectedRecipe = selectedRecipe[1].split("</td>")[0];
+        document.getElementById("second-page").style.display = "none";
+        document.getElementById("third-page").style.display = "block";
+  
+        for (let element of recipes) {
+          if (element["name"] == selectedRecipe) {
+            var elmnt = document.getElementById("recipeTitle");
+            elmnt.innerHTML = selectedRecipe;
+  
+            elmnt = document.getElementById("ingredientsInfo");
+            elmnt.innerHTML = "";
+  
+            var list = Object.values(element["ingredients"]);
+            
+            for (let x of list) {
+              if (x[2] == "") {
+                elmnt.innerHTML += x[1] + " " + x[0] + "<br />";
+              } else {
+                elmnt.innerHTML += x[1] + " " + x[2] + " of " + x[0] + "<br />";
+              }
+            }
+  
+            elmnt = document.getElementById("instructionsInfo");
+            elmnt.innerHTML = "";
+  
+            list = Object.values(element["instructions"]);
+  
+            for (let x of list) {
+              elmnt.innerHTML += x + "<br />";
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+document.getElementById("back-to-first-button").onclick = function() {
+  document.getElementById("first-page").style.display = "block";
+  document.getElementById("second-page").style.display = "none";
+}
+
+document.getElementById("back-to-second-button").onclick = function() {
+  document.getElementById("second-page").style.display = "block";
+  document.getElementById("third-page").style.display = "none";
+}
+
+document.getElementById("print").onclick = function() {
+  print();
+}
 
 getAllInfo(recipes);
 generateTable(ingredientsList);
-possibleRecipesTable(ingredientsList);
